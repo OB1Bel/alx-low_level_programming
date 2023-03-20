6c4a6ba390886c2a647b6b6677169e0983d84698
OB1Bel <ob1belaissaoui@gmail.com>
2023-03-20 15:33:20 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m < 100; m++)
+	{
+		for (n = 0; n < 100; n++)
+		{
+			if (m < n)
+			{
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				putchar(' ');
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				if (m == 98 || n == 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}

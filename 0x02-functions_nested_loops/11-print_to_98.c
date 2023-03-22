97e75dbc3f4bcc5f8f3eb510946eddd9da4cbe29
OB1Bel <ob1belaissaoui@gmail.com>
2023-03-22 13:56:17 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints numbers to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: start print from this number
  *
  * Return: Always 0

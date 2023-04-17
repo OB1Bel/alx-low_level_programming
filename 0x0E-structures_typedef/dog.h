36436432246f439c0975aa475de430ee159b5055
OB1Bel <ob1belaissaoui@gmail.com>
2023-04-17 09:35:55 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -20,7 +19,6 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
